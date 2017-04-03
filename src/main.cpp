99739b53eb7f50c10796c98af9b83d19bc07b711
joelnir <joel.oscarsson@hotmail.com>
2017-04-03 19:12:12 +0200

    Set up makefile with temporary c++ files. Added hashing library and set up gitignore,

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "hashlibpp.h"
+#include "test1.h"
+
+using namespace std;
+
+int main(){
+    test_funct();
+}
---
+++ src/test1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "test1.h"
+
+using namespace std;
+
+void test_funct(){
+    cout << "from 3rd test" << endl;
+}
