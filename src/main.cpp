2c811b396e25697923fe9c409e95493f0d6b066f
joelnir <joel.oscarsson@hotmail.com>
2017-04-11 22:04:59 +0200

    Fixed connection to the hashlib++ library.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
-#include "hashlibpp.h"
+#include <string>
+#include <hashlibpp.h>
 #include "test1.h"
 
 using namespace std;
 
 int main(){
-    test_funct();
+    //test_funct();
+
+    string s = "string to hash1";
+    hashwrapper *wrap = new md5wrapper();
+    string m5Hash = wrap->getHashFromString(s);
+
+    cout << "Hash: " << m5Hash << endl;
+
+    delete wrap;
 }
